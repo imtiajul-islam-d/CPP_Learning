85cf1c510d1a62dcf7dcccacc75871a4cd6d0e62
imtiajul-islam-d <imtiajul@hotmail.com>
2023-05-21 08:36:35 +0600

    input output in c++

---
+++ m1/input_output.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // std::cout<<"Hello C++ new";
+    // print space in c++
+    // std::cout<<"Hello "<<"Space";
+    // print new line in c++
+    // std::cout<<"Hello\nNew line old";
+    // std::cout<<"Hello" <<std::endl <<"New line";
+    // print variable in c++
+    int n = 10;
+    int m = 20;
+    cout<<n <<" " <<m;
+    return 0;
+}
