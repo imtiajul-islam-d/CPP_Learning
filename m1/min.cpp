d6b958f6a0b394b866a68ff4074e1e267dc671be
imtiajul-islam-d <imtiajul@hotmail.com>
2023-05-21 10:52:08 +0600

    swap function in c++

--- m1/min.cpp
+++ m1/min.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
-int min(int a, int b)
-{
-    if(a>b) return b;
-    else return a;
-}
+// int min(int a, int b)
+// {
+//     if(a>b) return b;
+//     else return a;
+// }
 int main()
 {
     int a, b;
     cin>> a >> b;
     int min_number = min(a, b);
-    cout<< min_number;
+    int max_number = max(a, b);
+    cout<< min_number << " " << max_number;
     return 0;
 }
