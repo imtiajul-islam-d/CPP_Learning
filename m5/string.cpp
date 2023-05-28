f2e1eac8adee6160c1daa84048f75d67eb066065
imtiajul-islam-d <imtiajul@hotmail.com>
2023-05-28 20:04:13 +0600

    string in c++

---
+++ m5/string.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string emi = "Emi";
+    string eshat = "Eshat";
+    if(emi == eshat){
+        cout << "Same" << endl;
+    }
+    else
+    {
+        cout << "Not same" << endl;
+    }
+    return 0;
+}
