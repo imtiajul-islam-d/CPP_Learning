46b85f73743d333713303db171c68b38630c9208
imtiajul-islam-d <imtiajul@hotmail.com>
2023-05-29 21:21:58 +0600

    string constructor

---
+++ m6/constructor_string.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // string s = "First way to declare an object with string constructor";
+    // string s("Second way to declare an object with string constructor");
+    // string s("Third way to declare an object with string constructor", 10);
+    // string a = "Fourth way to declare an object with string constructor";
+    // string s(a, 10); // to declare an object with ....
+    string s(2, 'x');  // xx
+    cout << s << endl;
+    return 0;
+}
