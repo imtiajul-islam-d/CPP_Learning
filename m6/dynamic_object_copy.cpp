45f233d8b214442884f4f320970736ddaa619bb8
imtiajul-islam-d <imtiajul@hotmail.com>
2023-05-31 13:20:35 +0600

    dynamic object copy

---
+++ m6/dynamic_object_copy.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Person
+{
+public:
+    string name;
+    int age;
+    Person(string name, int age)
+    {
+        this->name = name;
+        this->age = age;
+    }
+};
+int main()
+{
+    Person *emi = new Person("Emi", 28);
+    Person *emtiaz = new Person("Emtiaz", 28);
+    // emi->name = emtiaz->name;
+    // emi->age = emtiaz->age;
+    *emi = *emtiaz;
+    cout << emi->name << endl;
+    return 0;
+}
