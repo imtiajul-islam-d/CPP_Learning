a3300621629aff99e9ba375347704fdcb4779899
imtiajul-islam-d <imtiajul@hotmail.com>
2023-05-31 12:17:15 +0600

    summation function inside class added

--- m6/function_class.cpp
+++ m6/function_class.cpp
@@ -14,11 +14,17 @@ class Person
     {
         cout << "Testing" << endl;
     }
+    int countAge(int one, int two)
+    {
+        return one + two;
+    }
 };
 int main()
 {
     Person emi("Imtiazul Islam", 28);
     cout << emi.name << " " << emi.age << endl;  
     emi.test();
+    int plus = emi.countAge(30, 50);
+    cout << plus << endl;
     return 0;
 }
