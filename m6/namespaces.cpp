6a7f6aa3259572bd4d6a8a5e1c1076333106aac8
imtiajul-islam-d <imtiajul@hotmail.com>
2023-05-31 15:29:18 +0600

    namespace

---
+++ m6/namespaces.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+namespace Emi
+{
+    string name = "Emi";
+}
+namespace Emtiaz
+{
+    string name = "Emtiaz";
+}
+using namespace Emtiaz;
+int main()
+{
+    cout << Emi::name << endl;
+    cout << name << endl;
+    return 0;
+}
